bc98132716be443e0e8bcb70553b3a40a65abddb
Maisie <miss.sonagi@gmail.com>
2017-05-09 16:54:13 +0900

    Copy django repo - this repo removed in github.

---
+++ django/README.md
@@ -0,0 +1,10 @@
+# Django
+장고 걸스 튜토리얼을 따라해 보았습니다.
+
+- <https://tutorial.djangogirls.org/ko/>
+- <https://djangogirls.gitbooks.io/django-girls-tutorial-extensions/>
+
+직설적이지만 설명이 부족해서, 오히려 헤맸던 부분이 있었지만
+그래도 이틀 안에 끝낼 수 있었습니다.
+
+다른 Django 책을 따라하는데에 있어서 기초 지식이 될 것 같습니다.
---
+++ django/blog/migrations/0002_auto_20170217_2026.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import django.utils.timezone
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='post',
+            name='published_date',
+            field=models.DateTimeField(blank=True, null=True),
+        ),
+        migrations.AlterField(
+            model_name='post',
+            name='created_date',
+            field=models.DateTimeField(default=django.utils.timezone.now),
+        ),
+    ]
