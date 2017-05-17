b1c99d0a5804d011c6b9333dcb1879cb3da0ab7a
Maisie <miss.sonagi@gmail.com>
2017-05-17 17:48:17 +0900

    Add some test lines.

--- KoNLP/test.py
+++ KoNLP/test.py
@@ -1,11 +1,14 @@
+""" test.py
+
+"""
 import tweepy
 from tweepy.streaming import StreamListener
 
 import sqlalchemy
-import auth
+from auth import C_KEY, C_SECRET, A_TOKEN, A_SECRET
 
 AUTH = tweepy.OAuthHandler(C_KEY, C_SECRET)
-AUTH.set_access_token(A_TOKEN_KEY, A_TOKEN_SECRET)
+AUTH.set_access_token(A_TOKEN, A_SECRET)
 
 API = tweepy.API(AUTH, wait_on_rate_limit=True,
                  wait_on_rate_limit_notify=True)
@@ -18,16 +21,19 @@ print(" * name: " + MY_NAME + " *")
 
 class Timeline(StreamListener):
     def on_status(self, status):
+        print('hello')
+        timelines = API.home_timeline()
+        for timeline in timelines:
+            print(timeline.text)
+        
         mentions = API.mentions_timeline(count=1)
         for mention in mentions:
             print(mention.text)
 
-        timelines = API.home_timeline()
-        for timeline in timelines:
-            print(timeline.text)
-
 
 listener = Timeline()
 stream = tweepy.Stream(auth=AUTH, listener=listener)
-stream.userstream(_with='user', async=False)
+stream.userstream()
 
+
+# TODO: It doesn't work. It can't holding itself.
