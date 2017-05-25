5c01458bfa4c9f082e0338e586300073e9cf3d13
Maisie <miss.sonagi@gmail.com>
2017-05-25 16:56:56 +0900

    Complete CRC - Edit Output

--- Java/Homework/src/CRC.java
+++ Java/Homework/src/CRC.java
@@ -4,39 +4,50 @@
 
 // Divisor: X^5 + X^4 + X^2 + 1 = 110101
 // Dividend: x^9 + x^7 + x^3 + x^2 + 1 = 1010001101
-    //Test Divisor: 1011
-    //Test Dividend: 1001 + 000
 
 class Encoder{
     private int[] divisor; // Fixed-number array
     private int[] dividend; // Flexible-number array - It changes as the calculation progresses.
-    private int[] result;
+    private int[] remainder;
+    private int[] codeword;
 
     public Encoder(int array[]){
         setDivisor();
         setDividend(array);
-        setResult();
+        setRemainder();
     }
 
-    private void setDivisor(){
-        this.divisor = new int[6];
-        for(int i = 0; i < this.divisor.length; i++)
+    private void setDivisor(){ // Fix Divisor's data & size.
+        divisor = new int[6];
+        for(int i = 0; i < divisor.length; i++)
             if((i == 0)||(i == 1)||(i == 3)||(i == 5))
-                this.divisor[i] = 1;
+                divisor[i] = 1;
     }
 
-    private void setDividend(int array[]){
+    private void setDividend(int array[]){ // Fix Dividend's size.
         dividend = new int[array.length + divisor.length - 1];
         for(int i = 0 ; i < array.length; i++) {
             dividend[i] = array[i];
         }
     }
 
-    private void setResult(){
-        this.result = new int[this.divisor.length - 1];
+    private void setRemainder(){
+        // Reminder = n - 1, n = divisor's length.
+        remainder = new int[divisor.length - 1];
+    }
+
+    private void setCodeword(int[] array, int[] remainder){
+        codeword = new int[dividend.length];
+        for(int i = 0; i < array.length;i++){
+            codeword[i] = array[i];
+        }
+        for(int i = 0; i < remainder.length; i++){
+            codeword[i+array.length] = remainder[i];
+        }
     }
 
     private int XOR(int x, int y){
+        // It's just XOR calculator.
         if(x == y){
             return 0;
         }else{
@@ -44,28 +55,33 @@ class Encoder{
         }
     }
 
-    public int[] calculate(){
-        for(int i = 0; i + divisor.length -1 < this.dividend.length; i++) {
-            System.out.println("dividend:" + dividend[i] + dividend[i + 1] + dividend[i + 2] + dividend[i + 3] + dividend[i+4] + dividend[i+5]);
-            int j = 0;
+    public int[] calculate(int[] array){
+        for(int i = 0; i + divisor.length -1 < dividend.length; i++) {
+            // When the end of divisor array touches the end of dividend array, the loop ends.
+            int j = 0; // This integer variables shared by two loops.
             if (dividend[i] == 0) {
-                for (; j < this.divisor.length; j++) {
-                        System.out.println("divisor: 0!!");
+                // Dividend starts 0 >> Uses 0 divisor.
+                for (; j < divisor.length; j++) {
                         dividend[i + j] = XOR(0, dividend[i + j]);
-                        System.out.println("dividend[" + (i + j) + "]: " + dividend[i + j]);
                 }
             } else {
-                for (; j < this.divisor.length; j++) {
-                    System.out.println("divisor: " + divisor[j]);
+                // Dividend starts 1 >> Uses fixed divisor.
+                for (; j < divisor.length; j++) { //
                     dividend[i + j] = XOR(divisor[j], dividend[i + j]);
                 }
             }
         }
 
-        for(int i = 0; i < dividend.length;i++){
-            System.out.print(dividend[i] + " ");
+        for(int i = 0; i < dividend.length - remainder.length; i++)
+            System.out.print(dividend[i]);
+
+        for(int i = 0; i < remainder.length; i++){
+            // Now result array gets result from dividend.
+            remainder[i] = dividend[i + dividend.length - remainder.length];
         }
-        return this.result;
+
+        setCodeword(array, remainder);
+        return codeword;
     }
 }
 
@@ -73,6 +89,14 @@ public class CRC {
     public static void main(String args[]){
         int[] array = {1, 0, 1, 0, 0, 0, 1, 1, 0, 1};
         Encoder encoder = new Encoder(array);
-        encoder.calculate();
+        int remainder[] = encoder.calculate(array);
+
+        System.out.print("The remainder: ");
+        for(int i = 0; i < remainder.length; i++){
+            System.out.print(remainder[i] + " ");
+            if (i == 9){
+                System.out.print(" ");
+            }
+        }
     }
 }
